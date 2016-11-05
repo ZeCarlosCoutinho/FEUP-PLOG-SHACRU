477ba9e152767563cc96d7b978ab919232e779c4
ZeCarlosCoutinho <jczelik@gmail.com>
2016-11-05 20:46:08 +0000

    - getLine implemented and tested

--- trabalho.pl
+++ trabalho.pl
@@ -66,7 +66,7 @@ displayBoard([Row|Rest], Iterator):-
 	IteratorPlus is Iterator + 1,
 	printRow(Row),
 	printRowSeparator,
-	displayBoard(Rest, IteratorPlus).
+	displayBoard(Rest, IteratorPlus).	
 
 printBoard:-
 	printDownBorder, %experiencia
@@ -93,8 +93,35 @@ colocaMarcador(Jogador, Casa).
 giraPeca(Jogador, Peca, Direcao). % 0 - Nao gira, 1 - 45º contra-relogio, 2 - 45º sentido relogio
 podeMoverPeca(Jogador, Peca, Direcao). %- Verifica se uma peca pode ser movida na direcao escolhida
 mudouDeSeccao(Jogador, Peca, Direcao). %- Verifica se uma peca mudou de seccao
-alcancavel(Jogador, Peca, Casa),. %- Verifica se, nesse momento, é possivel determinada peça, com um numero qualquer de jogadaschegar a uma determinada casa
+alcancavel(Jogador, Peca, Casa). %- Verifica se, nesse momento, é possivel determinada peça, com um numero qualquer de jogadaschegar a uma determinada casa
 casaTemPeca(Casa). %
 casaTemMarcadorOposto(Jogador, Casa). %
 contaMarcadores(Jogador, Tabuleiro, Contador). %
 fimDeJogo. %
+
+getLine(Board, Line, LineNumber):- getLine(Board, Line, LineNumber, 1). % Iterator = 1
+getLine([Line|Rest], ResultLine, LineNumber, LineNumber):- % When Iterator = LineNumber
+	write('Iterator = LineNumber'), nl,
+	ResultLine = Line.
+getLine([Line|Rest], ResultLine, LineNumber, Iterator):- % Iterates across the lines of the board
+	write('Start'), nl, 
+	IteratorPlus is Iterator + 1,
+	write('Iterator + 1'), nl,
+	getLine(Rest, ResultLine, LineNumber, IteratorPlus).
+	
+% getTile(Board, Tile):-
+showLine(LineNumber):-
+	T=	[[[2, 0],[0, 0],[1, 0],[1, 2],[1, 2],[0, 0],[2, 0],[2, 0],[1, 3]],
+		[[0, 0],[2, 0],[1, 0],[1, 0],[1, 0],[2, 0],[2, 0],[1, 0],[2, 0]],
+		[[0, 0],[2, 0],[1, 0],[1, 0],[1, 0],[2, 0],[2, 0],[1, 0],[2, 0]],
+		[[0, 0],[2, 0],[1, 0],[0, 0],[1, 0],[2, 0],[2, 0],[1, 0],[2, 0]],
+		[[1, 0],[1, 0],[2, 0],[1, 9],[2, 0],[1, 0],[2, 0],[1, 0],[2, 0]],
+		[[2, 4],[2, 0],[1, 0],[2, 0],[2, 0],[1, 0],[1, 0],[2, 0],[2, 6]],
+		[[0, 0],[0, 0],[2, 0],[1, 0],[1, 0],[2, 0],[1, 0],[2, 0],[2, 6]],
+		[[0, 0],[0, 0],[2, 0],[1, 0],[1, 0],[1, 0],[2, 0],[1, 0],[2, 9]],
+		[[0, 0],[0, 0],[1, 0],[2, 0],[2, 0],[2, 0],[2, 0],[0, 0],[1, 0]]],
+	getLine(T, Line, LineNumber),
+	write(Line).
+	
+	
+jogadasValidas(Tabuleiro, Jogador, Peca, []).
