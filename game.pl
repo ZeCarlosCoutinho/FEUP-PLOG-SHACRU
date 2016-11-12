6e5e467c9a1bd366ab6bea6d149896e10e34dc76
ZeCarlosCoutinho <jczelik@gmail.com>
2016-11-12 14:38:34 +0000

    - createPlayerScores implemented and tested

--- game.pl
+++ game.pl
@@ -5,7 +5,17 @@ askForNumPlayers(NumPlayers):-
 	read(NumPlayersTemp), nl,
 	NumPlayersTemp > 1, NumPlayersTemp < 5,
 	NumPlayers = NumPlayersTemp .
+
+createPlayersScoreStructure(NumPlayers, ScoreStructure):- createPlayersScoreStructure(NumPlayers, [], 1, ScoreStructure).
+createPlayersScoreStructure(NumPlayers, TemporaryStructure, Iterator, ScoreStructure):- %Creates the ScoreStructure after all player scores are created
+	Iterator > NumPlayers,
+	ScoreStructure = TemporaryStructure.
+createPlayersScoreStructure(NumPlayers, TemporaryStructure, Iterator, ScoreStructure):-
+	IteratorPlus is Iterator + 1,
+	append(TemporaryStructure, [[Iterator, 0]], NewTemporaryStructure),
+	createPlayersScoreStructure(NumPlayers, NewTemporaryStructure, IteratorPlus, ScoreStructure).
 	
-testAsk:-
-	askForNumPlayers(Num),
-	write(Num).
+createPlayerScores(ScoreStructure):-
+	askForNumPlayers(NumPlayers),
+	createPlayersScoreStructure(NumPlayers, ScoreStructure).
+	
